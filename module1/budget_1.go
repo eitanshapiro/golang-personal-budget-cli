7474502e459a6cb5086bfea31a703f7cecff3ad2
SHAPIRO Eitan <eitan.shapiro@orca.ent>
2020-02-20 12:27:20 +0200

    add structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	max float32
+	items []Item
+}
 
 // Item stores item information
+type Item struct {
+	description string
+	price float32
+}
